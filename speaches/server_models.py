db7bf9a8a31b91144a5c0a3e6043ab21db527770
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-05-22 22:09:42 -0700

    feat: improve openai compatability

--- speaches/server_models.py
+++ speaches/server_models.py
@@ -1,26 +1,58 @@
+from __future__ import annotations
+
 import enum
 
+from faster_whisper.transcribe import Segment, Word
 from pydantic import BaseModel
 
-from speaches.core import Word
+from speaches.core import Transcription
 
 
+# https://platform.openai.com/docs/api-reference/audio/createTranscription#audio-createtranscription-response_format
 class ResponseFormat(enum.StrEnum):
-    JSON = "json"
     TEXT = "text"
+    JSON = "json"
     VERBOSE_JSON = "verbose_json"
+    # VTT = "vtt"
+    # SRT = "srt"
 
 
 # https://platform.openai.com/docs/api-reference/audio/json-object
-class TranscriptionResponse(BaseModel):
+class TranscriptionJsonResponse(BaseModel):
     text: str
 
+    @classmethod
+    def from_transcription(
+        cls, transcription: Transcription
+    ) -> TranscriptionJsonResponse:
+        return cls(text=transcription.text)
 
-# Subset of https://platform.openai.com/docs/api-reference/audio/verbose-json-object
-class TranscriptionVerboseResponse(BaseModel):
+
+# https://platform.openai.com/docs/api-reference/audio/verbose-json-object
+class TranscriptionVerboseJsonResponse(BaseModel):
     task: str = "transcribe"
+    language: str
     duration: float
     text: str
-    words: list[
-        Word
-    ]  # Different from OpenAI's `words`. `Word.text` instead of `Word.word`
+    words: list[Word]
+    segments: list[Segment]
+
+    @classmethod
+    def from_transcription(
+        cls, transcription: Transcription
+    ) -> TranscriptionVerboseJsonResponse:
+        return cls(
+            language="english",  # FIX: hardcoded
+            duration=transcription.duration,
+            text=transcription.text,
+            words=[
+                Word(
+                    start=word.start,
+                    end=word.end,
+                    word=word.text,
+                    probability=word.probability,
+                )
+                for word in transcription.words
+            ],
+            segments=[],  # FIX: hardcoded
+        )
