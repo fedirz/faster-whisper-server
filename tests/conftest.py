1bcb36153c0fc9677050985a67ad28505fb57471
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-06-22 15:17:31 -0700

    misc: tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import logging
+import os
+from typing import Generator
+
+import pytest
+from fastapi.testclient import TestClient
+
+# HACK
+os.environ["WHISPER_MODEL"] = "Systran/faster-whisper-tiny.en"
+from faster_whisper_server.main import app  # noqa: E402
 
 disable_loggers = ["multipart.multipart", "faster_whisper"]
 
@@ -7,3 +16,9 @@ def pytest_configure():
     for logger_name in disable_loggers:
         logger = logging.getLogger(logger_name)
         logger.disabled = True
+
+
+@pytest.fixture()
+def client() -> Generator[TestClient, None, None]:
+    with TestClient(app) as client:
+        yield client
