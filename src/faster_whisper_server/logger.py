300e422f339449a6abb7079f44cc8c9fd5be0079
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-09-08 11:00:44 -0700

    chore: set root logger level to info

--- src/faster_whisper_server/logger.py
+++ src/faster_whisper_server/logger.py
@@ -2,10 +2,7 @@ import logging
 
 from faster_whisper_server.config import config
 
-# Disables all but `faster_whisper_server` logger
-
-root_logger = logging.getLogger()
-root_logger.setLevel(logging.CRITICAL)
+logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(config.log_level.upper())
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s")
