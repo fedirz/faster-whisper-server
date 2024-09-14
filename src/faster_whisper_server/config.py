91a19d13ccc455dd0afb4215f6ffb755f02de8dc
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-09-14 19:14:14 +0200

    docs: mention quantization example for env variable

--- src/faster_whisper_server/config.py
+++ src/faster_whisper_server/config.py
@@ -170,7 +170,7 @@ class Config(BaseSettings):
 
     Pydantic will automatically handle mapping uppercased environment variables to the corresponding fields.
     To populate nested, the environment should be prefixed with the nested field name and an underscore. For example,
-    the environment variable `LOG_LEVEL` will be mapped to `log_level`, `WHISPER__MODEL`(note the double underscore) to `whisper.model`, etc.
+    the environment variable `LOG_LEVEL` will be mapped to `log_level`, `WHISPER__MODEL`(note the double underscore) to `whisper.model`, to set quantization to int8, use `WHISPER__COMPUTE_TYPE=int8`, etc.
     """  # noqa: E501
 
     model_config = SettingsConfigDict(env_nested_delimiter="__")
