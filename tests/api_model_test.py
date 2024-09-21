0c1fc004ae033ca590262e2116d7ac7b16a66a16
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-09-21 10:47:38 -0700

    test: switch to async openai client

--- tests/api_model_test.py
+++ tests/api_model_test.py
@@ -1,5 +1,5 @@
 import openai
-from openai import OpenAI
+from openai import AsyncOpenAI
 import pytest
 
 MODEL_THAT_EXISTS = "Systran/faster-whisper-tiny.en"
@@ -7,16 +7,19 @@ MODEL_THAT_DOES_NOT_EXIST = "i-do-not-exist"
 MIN_EXPECTED_NUMBER_OF_MODELS = 70  # At the time of the test creation there are 89 models
 
 
-def test_list_models(openai_client: OpenAI) -> None:
-    models = openai_client.models.list().data
+@pytest.mark.asyncio()
+async def test_list_models(openai_client: AsyncOpenAI) -> None:
+    models = (await openai_client.models.list()).data
     assert len(models) > MIN_EXPECTED_NUMBER_OF_MODELS
 
 
-def test_model_exists(openai_client: OpenAI) -> None:
-    model = openai_client.models.retrieve(MODEL_THAT_EXISTS)
+@pytest.mark.asyncio()
+async def test_model_exists(openai_client: AsyncOpenAI) -> None:
+    model = await openai_client.models.retrieve(MODEL_THAT_EXISTS)
     assert model.id == MODEL_THAT_EXISTS
 
 
-def test_model_does_not_exist(openai_client: OpenAI) -> None:
+@pytest.mark.asyncio()
+async def test_model_does_not_exist(openai_client: AsyncOpenAI) -> None:
     with pytest.raises(openai.NotFoundError):
-        openai_client.models.retrieve(MODEL_THAT_DOES_NOT_EXIST)
+        await openai_client.models.retrieve(MODEL_THAT_DOES_NOT_EXIST)
