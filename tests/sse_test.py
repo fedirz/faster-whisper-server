42fafd9802e2c4e53fd548c6da09337d19018919
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-09-30 08:30:24 -0700

    test: switch to async http client

--- tests/sse_test.py
+++ tests/sse_test.py
@@ -1,12 +1,13 @@
 import json
 import os
 
-from fastapi.testclient import TestClient
+import anyio
 from faster_whisper_server.api_models import (
     CreateTranscriptionResponseJson,
     CreateTranscriptionResponseVerboseJson,
 )
-from httpx_sse import connect_sse
+from httpx import AsyncClient
+from httpx_sse import aconnect_sse
 import pytest
 import srt
 import webvtt
@@ -22,57 +23,61 @@ ENDPOINTS = [
 parameters = [(file_path, endpoint) for endpoint in ENDPOINTS for file_path in FILE_PATHS]
 
 
+@pytest.mark.asyncio()
 @pytest.mark.parametrize(("file_path", "endpoint"), parameters)
-def test_streaming_transcription_text(client: TestClient, file_path: str, endpoint: str) -> None:
+async def test_streaming_transcription_text(aclient: AsyncClient, file_path: str, endpoint: str) -> None:
     extension = os.path.splitext(file_path)[1]
-    with open(file_path, "rb") as f:
-        data = f.read()
+    async with await anyio.open_file(file_path, "rb") as f:
+        data = await f.read()
     kwargs = {
         "files": {"file": (f"audio.{extension}", data, f"audio/{extension}")},
         "data": {"response_format": "text", "stream": True},
     }
-    with connect_sse(client, "POST", endpoint, **kwargs) as event_source:
-        for event in event_source.iter_sse():
+    async with aconnect_sse(aclient, "POST", endpoint, **kwargs) as event_source:
+        async for event in event_source.aiter_sse():
             print(event)
             assert len(event.data) > 1  # HACK: 1 because of the space character that's always prepended
 
 
+@pytest.mark.asyncio()
 @pytest.mark.parametrize(("file_path", "endpoint"), parameters)
-def test_streaming_transcription_json(client: TestClient, file_path: str, endpoint: str) -> None:
+async def test_streaming_transcription_json(aclient: AsyncClient, file_path: str, endpoint: str) -> None:
     extension = os.path.splitext(file_path)[1]
-    with open(file_path, "rb") as f:
-        data = f.read()
+    async with await anyio.open_file(file_path, "rb") as f:
+        data = await f.read()
     kwargs = {
         "files": {"file": (f"audio.{extension}", data, f"audio/{extension}")},
         "data": {"response_format": "json", "stream": True},
     }
-    with connect_sse(client, "POST", endpoint, **kwargs) as event_source:
-        for event in event_source.iter_sse():
+    async with aconnect_sse(aclient, "POST", endpoint, **kwargs) as event_source:
+        async for event in event_source.aiter_sse():
             CreateTranscriptionResponseJson(**json.loads(event.data))
 
 
+@pytest.mark.asyncio()
 @pytest.mark.parametrize(("file_path", "endpoint"), parameters)
-def test_streaming_transcription_verbose_json(client: TestClient, file_path: str, endpoint: str) -> None:
+async def test_streaming_transcription_verbose_json(aclient: AsyncClient, file_path: str, endpoint: str) -> None:
     extension = os.path.splitext(file_path)[1]
-    with open(file_path, "rb") as f:
-        data = f.read()
+    async with await anyio.open_file(file_path, "rb") as f:
+        data = await f.read()
     kwargs = {
         "files": {"file": (f"audio.{extension}", data, f"audio/{extension}")},
         "data": {"response_format": "verbose_json", "stream": True},
     }
-    with connect_sse(client, "POST", endpoint, **kwargs) as event_source:
-        for event in event_source.iter_sse():
+    async with aconnect_sse(aclient, "POST", endpoint, **kwargs) as event_source:
+        async for event in event_source.aiter_sse():
             CreateTranscriptionResponseVerboseJson(**json.loads(event.data))
 
 
-def test_transcription_vtt(client: TestClient) -> None:
-    with open("audio.wav", "rb") as f:
-        data = f.read()
+@pytest.mark.asyncio()
+async def test_transcription_vtt(aclient: AsyncClient) -> None:
+    async with await anyio.open_file("audio.wav", "rb") as f:
+        data = await f.read()
     kwargs = {
         "files": {"file": ("audio.wav", data, "audio/wav")},
         "data": {"response_format": "vtt", "stream": False},
     }
-    response = client.post("/v1/audio/transcriptions", **kwargs)
+    response = await aclient.post("/v1/audio/transcriptions", **kwargs)
     assert response.status_code == 200
     assert response.headers["content-type"] == "text/vtt; charset=utf-8"
     text = response.text
@@ -82,14 +87,15 @@ def test_transcription_vtt(client: TestClient) -> None:
         webvtt.from_string(text)
 
 
-def test_transcription_srt(client: TestClient) -> None:
-    with open("audio.wav", "rb") as f:
-        data = f.read()
+@pytest.mark.asyncio()
+async def test_transcription_srt(aclient: AsyncClient) -> None:
+    async with await anyio.open_file("audio.wav", "rb") as f:
+        data = await f.read()
     kwargs = {
         "files": {"file": ("audio.wav", data, "audio/wav")},
         "data": {"response_format": "srt", "stream": False},
     }
-    response = client.post("/v1/audio/transcriptions", **kwargs)
+    response = await aclient.post("/v1/audio/transcriptions", **kwargs)
     assert response.status_code == 200
     assert "text/plain" in response.headers["content-type"]
 
