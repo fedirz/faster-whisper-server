68b9f6d3e558fcafe8e81f3977727c1d2ee7b019
Shubham Kushwaha <65639964+EricLiclair@users.noreply.github.com>
2024-10-19 20:34:44 +0530

    setup_logger to use config.log_level for basicConfig
    patches the faster_whisper_serve.logger#setup_logger to use config.log_level to setup the basic config.
    also clears unnecessary initialisations of logger (as we are not returning any logger from the method)
    
    afaik, the logger created, with logging.getLogger, falls back to the root logger's handlers if no handler is specified for the created logger itself.
    
    If need be, the `logging.root.handler.clear()` method could be used to clear all pre initialized handlers on the logger before (re)setting it.

--- src/faster_whisper_server/logger.py
+++ src/faster_whisper_server/logger.py
@@ -5,7 +5,4 @@ from faster_whisper_server.dependencies import get_config
 
 def setup_logger() -> None:
     config = get_config()  # HACK
-    logging.getLogger().setLevel(logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(config.log_level.upper())
-    logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s")
+    logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s", level=config.log_level.upper())
