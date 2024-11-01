dcbab06cf2a4cb7fae393cc6a39b4c7de5fa44ff
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2024-11-01 08:05:13 -0700

    feat: token based auth

--- src/faster_whisper_server/dependencies.py
+++ src/faster_whisper_server/dependencies.py
@@ -1,7 +1,8 @@
 from functools import lru_cache
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 from faster_whisper_server.config import Config
 from faster_whisper_server.model_manager import PiperModelManager, WhisperModelManager
@@ -31,3 +32,16 @@ def get_piper_model_manager() -> PiperModelManager:
 
 
 PiperModelManagerDependency = Annotated[PiperModelManager, Depends(get_piper_model_manager)]
+
+
+security = HTTPBearer()
+
+
+async def verify_api_key(
+    config: ConfigDependency, credentials: Annotated[HTTPAuthorizationCredentials, Depends(security)]
+) -> None:
+    if credentials.credentials != config.api_key:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+
+ApiKeyDependency = Depends(verify_api_key)
