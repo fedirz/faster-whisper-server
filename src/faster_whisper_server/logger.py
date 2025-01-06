836a3955cacb14e2f6b57903fea906f32fb7014b
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-01-06 09:19:58 -0800

    use dict to configure the logger (also fixes #114)

--- src/faster_whisper_server/logger.py
+++ src/faster_whisper_server/logger.py
@@ -1,8 +1,30 @@
 import logging
+import logging.config
 
 
 def setup_logger(log_level: str) -> None:
-    logging.getLogger().setLevel(logging.INFO)
-    logger = logging.getLogger(__name__)
-    logger.setLevel(log_level.upper())
-    logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(lineno)d:%(message)s")
+    assert log_level.upper() in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], log_level
+    # https://www.youtube.com/watch?v=9L77QExPmI0
+    # https://docs.python.org/3/library/logging.config.html
+    logging_config = {
+        "version": 1,  # required
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {"format": "%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(lineno)d:%(message)s"},
+        },
+        "handlers": {
+            "stdout": {
+                "class": "logging.StreamHandler",
+                "formatter": "simple",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "root": {
+                "level": log_level.upper(),
+                "handlers": ["stdout"],
+            },
+        },
+    }
+
+    logging.config.dictConfig(logging_config)
