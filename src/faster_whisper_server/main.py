7785332bb6a3e65c985df3a83ae8572a5ac4014f
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-01-09 18:20:36 -0800

    feat: add API tags

--- src/faster_whisper_server/main.py
+++ src/faster_whisper_server/main.py
@@ -12,11 +12,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from faster_whisper_server.dependencies import ApiKeyDependency, get_config, get_model_manager
 from faster_whisper_server.logger import setup_logger
-from faster_whisper_server.routers.list_models import (
-    router as list_models_router,
-)
 from faster_whisper_server.routers.misc import (
     router as misc_router,
+)
+from faster_whisper_server.routers.models import (
+    router as models_router,
 )
 from faster_whisper_server.routers.stt import (
     router as stt_router,
@@ -24,6 +24,19 @@ from faster_whisper_server.routers.stt import (
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
+
+# https://swagger.io/docs/specification/v3_0/grouping-operations-with-tags/
+# https://fastapi.tiangolo.com/tutorial/metadata/#metadata-for-tags
+TAGS_METADATA = [
+    {"name": "automatic-speech-recognition"},
+    {"name": "speech-to-text"},
+    {"name": "models"},
+    {"name": "diagnostic"},
+    {
+        "name": "experimental",
+        "description": "Not meant for public use yet. May change or be removed at any time.",
+    },
+]
 
 
 def create_app() -> FastAPI:
@@ -53,10 +66,10 @@ def create_app() -> FastAPI:
     if config.api_key is not None:
         dependencies.append(ApiKeyDependency)
 
-    app = FastAPI(lifespan=lifespan, dependencies=dependencies)
+    app = FastAPI(lifespan=lifespan, dependencies=dependencies, openapi_tags=TAGS_METADATA)
 
     app.include_router(stt_router)
-    app.include_router(list_models_router)
+    app.include_router(models_router)
     app.include_router(misc_router)
     if speech_router is not None:
         app.include_router(speech_router)
