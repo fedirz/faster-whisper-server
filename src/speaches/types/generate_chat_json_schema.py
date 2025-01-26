abf6e9196ad9a7c4d0dd8ee1c6c201ae8344705f
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-01-26 11:41:42 -0800

    feat: create pydantic model for chat completion params
    Couldn't just use the type exported by `openai` because of these issues:
    - https://github.com/pydantic/pydantic/issues/9467
    - https://github.com/pydantic/pydantic/issues/9541

---
+++ src/speaches/types/generate_chat_json_schema.py
@@ -0,0 +1,10 @@
+import json
+
+from openai.types.chat.completion_create_params import CompletionCreateParamsBase
+from pydantic import TypeAdapter
+
+adapter = TypeAdapter(CompletionCreateParamsBase)
+
+print(json.dumps(adapter.json_schema()))
+
+# python src/speaches/types/generate_chat_json_schema.py | datamodel-codegen  --input-file-type jsonschema --target-python-version 3.12 --output-model-type pydantic_v2.BaseModel --use-standard-collections --use-union-operator --enum-field-as-literal all > src/speaches/types/chat.py && ruff check --fix src/speaches/types/chat.py && ruff format src/speaches/types/chat.py  # noqa: E501
