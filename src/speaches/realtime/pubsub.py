e5a6eca46bccc68ac4a4ccb278d64219b4db12dd
Fedir Zadniprovskyi <github.g1k56@simplelogin.com>
2025-02-20 08:56:46 -0800

    refactor/realtime: create generic `PubSub` class

--- src/speaches/realtime/pubsub.py
+++ src/speaches/realtime/pubsub.py
@@ -4,31 +4,46 @@ import json
 import logging
 from pathlib import Path
 
+from pydantic import BaseModel
+
 from speaches.types.realtime import CLIENT_EVENT_TYPES, SERVER_EVENT_TYPES, Event
 
 logger = logging.getLogger(__name__)
 
 
-class EventPubSub:
+class PubSub[T: BaseModel]:
     def __init__(self) -> None:
-        self.subscribers: set[Queue[Event]] = set()
-        self.events: list[Event] = []  # to store all events
+        self.subscribers: set[Queue[T]] = set()
+        self.events: list[T] = []  # to store all events
 
-    async def publish(self, event: Event) -> None:
+    async def publish(self, event: T) -> None:
         self.events.append(event)
         for subscriber in self.subscribers:
             await subscriber.put(event)
 
-    def publish_nowait(self, event: Event) -> None:
+    def publish_nowait(self, event: T) -> None:
         self.events.append(event)
         for subscriber in self.subscribers:
             subscriber.put_nowait(event)
 
-    def subscribe(self) -> Queue[Event]:
-        subscriber = Queue[Event]()
+    def subscribe(self) -> Queue[T]:
+        subscriber = Queue[T]()
         self.subscribers.add(subscriber)
         return subscriber
 
+    async def poll(self) -> AsyncGenerator[T, None]:
+        subscriber = Queue[T]()
+        self.subscribers.add(subscriber)
+        try:
+            while True:
+                event = await subscriber.get()
+                yield event.model_copy()
+        finally:
+            self.subscribers.remove(subscriber)
+            logger.info("Subscriber removed")
+
+
+class EventPubSub(PubSub[Event]):
     async def subscribe_to(self, event_type: str) -> AsyncGenerator[Event, None]:
         if event_type not in SERVER_EVENT_TYPES | CLIENT_EVENT_TYPES:
             raise ValueError(f"Invalid event type: {event_type}")
@@ -42,17 +57,6 @@ class EventPubSub:
         finally:
             self.subscribers.remove(subscriber)
             logger.info(f"Subscriber for event type {event_type} removed")
-
-    async def poll(self) -> AsyncGenerator[Event, None]:
-        subscriber = Queue[Event]()
-        self.subscribers.add(subscriber)
-        try:
-            while True:
-                event = await subscriber.get()
-                yield event.model_copy()
-        finally:
-            self.subscribers.remove(subscriber)
-            logger.info("Subscriber removed")
 
     def dump_to_file(self, file_path: Path) -> None:
         with file_path.open("w") as f:
